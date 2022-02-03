93aca465955a126188a1aab52dfe07785aa59490
krungy <furler711@gmail.com>
2022-02-03 16:51:28 +0900

    💄 style: 구글 웹폰트 적용 & 페이지 타이틀 적용

--- src/styles/ResetStyle.jsx
+++ src/styles/ResetStyle.jsx
@@ -135,12 +135,12 @@ const ResetStyle = css`
   }
   * {
     box-sizing: border-box;
-    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui,
-      Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo',
-      'Noto Sans KR', 'Malgun Gothic', sans-serif;
   }
   html,
-  body,
+  body {
+    font-family: 'Noto Sans KR', sans-serif;
+    font-family: 'Roboto', sans-serif;
+  }
   #root {
     height: 100%;
   }
